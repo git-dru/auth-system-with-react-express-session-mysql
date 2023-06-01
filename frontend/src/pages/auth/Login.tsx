8ca2cabab34d10bfe575fea25208c370bffa9a0a
git-dru <mastercrypto.us@gmail.com>
2023-06-01 20:59:45 +0300

    add Login & Register page

--- frontend/src/pages/auth/Login.tsx
+++ frontend/src/pages/auth/Login.tsx
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Form, Button, Row, Col } from "react-bootstrap";
 
-const Login = () => {
-  return <></>;
+const LoginPage = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitHandler = () => {};
+
+  return (
+    <div>
+      <Row className="justify-content-md-center">
+        <Col xs={12} md={6}>
+          <h1>Sign In</h1>
+          <Form onSubmit={submitHandler}>
+            <Form.Group controlId="username">
+              <Form.Label>Username</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="enter username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="enter your password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Button type="submit" variant="primary">
+              Sign In
+            </Button>
+          </Form>
+
+          <Row className="py-3">
+            <Col>
+              Do not have an account?
+              <Link to={`/register`}> Register</Link>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </div>
+  );
 };
 
-export default Login;
+export default LoginPage;
